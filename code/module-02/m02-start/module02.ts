144a35d583bbd559eabc42b5f0d417999670ba04
SeaOtter(Ken) <kentaro.mori36@gmail.com>
2023-08-01 15:51:49 +0900

    fix

--- code/module-02/m02-start/module02.ts
+++ code/module-02/m02-start/module02.ts
@@ -5,36 +5,36 @@
     TODO: Modify the code to add types to the variable declarations. 
     The resulting JavaScript should look the same as the original example when you're done. */
 
-let firstName;
-let lastName;
-let fullName;
-let age;
-let ukCitizen;
-
-firstName = 'Rebecca';
-lastName = 'Smith';
-age = 42;
-ukCitizen = false;
-fullName = firstname + " " + lastname;
-
-if (ukCitizen) {
-    console.log("My name is " + fullName + ", I'm " + age + ", and I'm a citizen of the United Kingdom.");
-} else {
-    console.log("My name is " + fullName + ", I'm " + age + ", and I'm not a citizen of the United Kingdom.");
-}
+    let firstName: string;
+    let lastName: string;
+    let fullName: string;
+    let age: number;
+    let ukCitizen: boolean;
+    
+    firstName = 'Rebecca';
+    lastName = 'Smith';
+    age = 42;
+    ukCitizen = false;
+    fullName = firstName + " " + lastName;
+    
+    if (ukCitizen) {
+        console.log(`My name is ${fullName}, I'm ${age}, and I'm a citizen of the United Kingdom.`);
+    } else {
+        console.log(`My name is ${fullName}, I'm ${age}, and I'm not a citizen of the United Kingdom.`);
+    }
 
 /* EXERCISE 2
    TODO: You can use types to ensure operation outcomes. Run the code as is and then modify 
    it to have strongly typed variables. Then, address any errors you find so that the result 
    returned to a is 12. */
 
-let x;
-let y;
-let a;
+let x: string;
+let y: number;
+let a: string;
 
-x = 'five';
+x = '5';
 y = 7;
-a = x + y;
+a = parseInt(x) + y;
 
 console.log(a);
 
@@ -44,34 +44,42 @@ console.log(a);
    you can pass in the season by referencing an item in the enum, for example 
    Season.Fall, instead of the literal string "Fall". */
 
-function whichMonths(season) {
+   enum Season {
+    Fall = "Fall",
+    Winter = "Winter",
+    Spring = "Spring",
+    Summer = "Summer"
+}
 
+function whichMonths(season: Season) {
     let monthsInSeason: string;
 
     switch (season) {
-        case "Fall":
+        case Season.Fall:
             monthsInSeason = "September to November";
             break;
-        case "Winter":
+        case Season.Winter:
             monthsInSeason = "December to February";
             break;
-        case "Spring":
+        case Season.Spring:
             monthsInSeason = "March to May";
             break;
-        case "Summer":
+        case Season.Summer:
             monthsInSeason = "June to August";
+            break;
     }
 
-    return monthsInSeason;
+    console.log(`The months in ${season} are: ${monthsInSeason}`);
 }
 
-console.log(whichMonths("Fall"));
+whichMonths(Season.Fall);
 
 /* EXERCISE 4
    TODO: Declare the array as the type to match the type of the items in the array. */
 
-   let randomNumbers;
-   let nextNumber;
+
+   let randomNumbers: number[] = [];
+   let nextNumber: number;
    
    for (let i = 0; i < 10; i++) {
        nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
@@ -79,5 +87,3 @@ console.log(whichMonths("Fall"));
    }
    
    console.log(randomNumbers);
-
-   
